2576a74e9e6628917d38aacd1ae021e0f2c626c2
longfish <thomasmeng95@gmail.com>
2023-12-29 00:45:39 -0700

    implemented FEANet derivative

--- feanet/derivative.py
+++ feanet/derivative.py
@@ -7,30 +7,71 @@ class DerivativeNet(nn.Module):
     def __init__(self, chs, h):
         super(DerivativeNet, self).__init__()
         self.xNet_internal = nn.Conv2d(chs, chs, kernel_size=(1,3), padding=(0,1), bias=False, groups=chs)
-        self.xNet_edge = nn.Conv2d(chs, chs, kernel_size=(1,2), padding=0, bias=False, groups=chs)
+        self.xNet_left = nn.Conv2d(chs, chs, kernel_size=(1,3), padding=(0,1), bias=False, groups=chs)
+        self.xNet_right = nn.Conv2d(chs, chs, kernel_size=(1,3), padding=(0,1), bias=False, groups=chs)
         self.yNet_internal = nn.Conv2d(chs, chs, kernel_size=(3,1), padding=(1,0), bias=False, groups=chs)
-        self.yNet_edge = nn.Conv2d(chs, chs, kernel_size=(2,1), padding=0, bias=False, groups=chs)
+        self.yNet_top = nn.Conv2d(chs, chs, kernel_size=(3,1), padding=(1,0), bias=False, groups=chs)
+        self.yNet_bottom = nn.Conv2d(chs, chs, kernel_size=(3,1), padding=(1,0), bias=False, groups=chs)
         
-        self.xNet_internal.requires_grad_(False)
-        self.xNet_edge.requires_grad_(False)
-        self.yNet_internal.requires_grad_(False)
-        self.yNet_edge.requires_grad_(False)
-
         for i in range(chs):
             self.xNet_internal.state_dict()['weight'][i][0] = torch.asarray([-1., 0., 1.]) / (2.0 * h)
-            self.xNet_edge.state_dict()['weight'][i][0] = torch.asarray([-1., 1.]) / h
+            self.xNet_left.state_dict()['weight'][i][0] = torch.asarray([0., -1., 1.]) / h
+            self.xNet_right.state_dict()['weight'][i][0] = torch.asarray([-1., 1., 0.]) / h
             self.yNet_internal.state_dict()['weight'][i][0] = torch.asarray([[1.], [0.], [-1.]]) / (2.0 * h)
-            self.yNet_edge.state_dict()['weight'][i][0] = torch.asarray([[1.], [-1.]]) / h
+            self.yNet_top.state_dict()['weight'][i][0] = torch.asarray([[0.], [1.], [-1.]]) / h
+            self.yNet_bottom.state_dict()['weight'][i][0] = torch.asarray([[1.], [-1.], [0.]]) / h
 
-    def forward(self, u, msk, direction):
+        self.xNet_internal.requires_grad_(False)
+        self.xNet_left.requires_grad_(False)
+        self.xNet_right.requires_grad_(False)
+        self.yNet_internal.requires_grad_(False)
+        self.yNet_top.requires_grad_(False)
+        self.yNet_bottom.requires_grad_(False)
+
+
+    def shrink_mask(self, msk, direction):
         if(direction == 'x'):
-            derivative = self.xNet_internal(u)
-            edge = self.xNet_edge(u)
-            derivative[:, :, :, 0] = edge[:, :, :, 0]
-            derivative[:, :, :, -1] = edge[:, :, :, -1]
+            kernel = torch.ones((1, 1, 1, 3), dtype=torch.float32)
+            conv_result = F.conv2d(msk, kernel, padding=(0,1))
+            shrink_mask = (conv_result == 3).float() #(ensure all three pixels were 'on')
         if(direction == 'y'):
-            derivative = self.yNet_internal(u)
-            edge = self.yNet_edge(u)
-            derivative[:, :, 0, :] = edge[:, :, 0, :]
-            derivative[:, :, -1, :] = edge[:, :, -1, :]
-        return derivative
+            kernel = torch.ones((1, 1, 3, 1), dtype=torch.float32)
+            conv_result = F.conv2d(msk, kernel, padding=(1,0))
+            shrink_mask = (conv_result == 3).float() #(ensure all three pixels were 'on')
+        return shrink_mask
+
+    def eroded_pixels(self, mask, direction):
+        if(direction == 'x'):
+            cumsum = torch.cumsum(mask, dim=-1) # the cumulative sum along the width dimension
+            edge1 = (cumsum == 1) # positions where cumsum changes from 0 to a positive value
+            row_max = cumsum.max(dim=-1, keepdim=True).values
+            edge2 = (cumsum == row_max) & mask
+        if(direction == 'y'):
+            cumsum = torch.cumsum(mask, dim=-2) # the cumulative sum along the height dimension
+            edge1 = (cumsum == 1) # positions where cumsum changes from 0 to a positive value
+            col_max = cumsum.max(dim=-2, keepdim=True).values
+            edge2 = (cumsum == col_max) & mask
+        return edge1, edge2
+
+    def forward(self, u, mask, direction):
+        '''
+        edge_mask = mask - eroded_mask
+        1- compute the internal derivative
+        2- compute the edge derivative
+        3- extract the internal derivative using eroded_mask
+        4- extract the edge derivative using edge_mask
+        5- add the internal and edge derivative together
+        '''
+        eroded_mask = self.shrink_mask(mask, direction)
+        edge1_mask, edge2_mask = self.eroded_pixels(mask, direction)
+
+        if(direction == 'x'):
+            internal_d = eroded_mask * self.xNet_internal(u)
+            left_d = edge1_mask * self.xNet_left(u)
+            right_d = edge2_mask * self.xNet_right(u)
+            return internal_d + left_d + right_d
+        if(direction == 'y'):
+            internal_d = eroded_mask * self.yNet_internal(u)
+            top_d = edge1_mask * self.yNet_top(u)
+            bottom_d = edge2_mask * self.yNet_bottom(u)
+            return internal_d + top_d + bottom_d
